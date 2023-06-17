aa97acfbed7c3810fcd870de69e26223c9128d3f
Little Bit <cthibodeau09@gmail.com>
2023-06-16 21:08:58 -0500

    moved file to correct location updated index

--- models/index.js
+++ models/index.js
@@ -1,8 +1,4 @@
-const router = require("express").Router();
-const userRoutes = require("./user");
-const thoughtRoutes = require("./thought");
+const User = require("./User");
+const Thought = require("./Thought");
 
-router.use("/users", userRoutes);
-router.use("/thoughts", thoughtRoutes);
-
-module.exports = router;
+module.exports = { User, Thought };
