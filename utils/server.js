41b597a6e12bf8e5a24543ca2e948db856d29c92
Little Bit <cthibodeau09@gmail.com>
2023-07-09 13:07:54 -0400

    Had to completely redo filoe structure as there were errors

--- utils/server.js
+++ utils/server.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const db = require("./config/connection");
-const routes = require("./routes");
+const { MongoClient } = require('mongodb');
+const routes = require("./routes/api");
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
